3d487c7504c534990403f743bd9fcd8694d3a2df
odan <odan3240@gmail.com>
2024-06-07 22:45:29 +0900

    run format

--- eslint.config.js
+++ eslint.config.js
@@ -8,5 +8,5 @@ export default tseslint.config(
     ignores: ["dist/*", "lib/*", "node_modules/*"],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
 );
